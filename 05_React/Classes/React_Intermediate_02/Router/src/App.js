493c715c7a001faee5dec88437ab46e914a46c08
aakashthapa12 <aakashtm12@gmail.com>
2024-02-02 14:52:08 +0530

    First Commit

---
+++ 05_React/Classes/React_Intermediate_02/Router/src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Support from './components/Support';
+import About from './components/About';
+import Labs from './components/Labs';
+import NotFound from './components/NotFound';
+// import { Link } from 'react-router-dom';
+import MainHeader from './components/MainHeader';
+
+function App() {
+  return (
+    <div className="App">
+      {/* To show navigation items in UI  */}
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/support">Support</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/labs">Labs</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+
+
+      <Routes>
+        {/* Single Route is created by Route Tag */}
+        <Route path="/" element={<MainHeader/>} >
+          {/* Default Route */}
+          <Route index element={<Home/>} />
+          <Route path="/support" element={<Support/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/labs" element={<Labs/>} />
+          <Route path="*" element={<NotFound/>} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
